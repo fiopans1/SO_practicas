f6ccb7c5c3b15a86f6462d31b63e122762d81bd9
Diego Suárez Ramos <45702360+fiopans1@users.noreply.github.com>
2021-10-26 20:02:04 +0200

    a ver si tenemos suerte

---
+++ memoria.h
@@ -0,0 +1,38 @@
+#ifndef __MEMORIA_H
+#define __MEMORIA_H
+//VARIABLES
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/utsname.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <ctype.h> 
+#include <errno.h>
+#include <sys/file.h>
+#include <dirent.h>
+#include <pwd.h>
+#include <grp.h>
+#include <dirent.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/mman.h>
+#include "memoria_list.h"
+
+
+#define RED "\x1b[31m"
+#define GREEN "\x1b[32m"
+#define BLUE "\x1b[34m"
+#define COLOR_RESET "\x1b[0m"
+typedef char *cadena;
+#define LEERCOMPLETO ((ssize_t)-1)
+
+
+//definicion de funcines:
+void malloc1(cadena trozos,int n,tListM *M);
+//funciones secundarias:
+void obt_hora(char hora[]);
+#endif
